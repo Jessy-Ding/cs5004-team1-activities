8f09aae4240aab5fb8cff5db5e6199270f2ccaf5
Jessy-Ding <89423283+Jessy-Ding@users.noreply.github.com>
2026-01-25 21:51:11 -0800

    CODE: CodingBat: Array-3 > fix34

--- module03/Mengyuan_Ding.java
+++
@@ -1,8 +0,0 @@
-/**
- * Java practice problem
- *
- * @author Mengyuan Ding
- */
-public class Test{
-
-        }
---
+++ module03/array_Mengyuan_Ding.java
@@ -0,0 +1,40 @@
+/**
+ * CodingBat: Array-3 > fix34
+ * <p>
+ * Return an array that contains exactly the same numbers as the given array, but rearranged so that every 3 is immediately followed by a 4.
+ * Do not move the 3's, but every other number may move.
+ * The array contains the same number of 3's and 4's, every 3 has a number after it that is not a 3, and a 3 appears in the array before any 4.
+ * </p>
+ * <pre>{@code
+ * fix34([1, 3, 1, 4]) → [1, 3, 4, 1]
+ * fix34([1, 3, 1, 4, 4, 3, 1]) → [1, 3, 4, 1, 1, 3, 4]
+ * fix34([3, 2, 2, 4]) → [3, 4, 2, 2]
+ * }</pre>
+ * @author Mengyuan Ding
+ * @date 2026-01-25
+ * @course CS5004 (Object-Oriented Programming)
+ */
+public int[] fix34(int[] nums) {
+        int fourIndex = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+                if (nums[i] == 3) {
+
+                        // find the next 4 that is NOT already after a 3
+                        while (nums[fourIndex] != 4) {
+                                fourIndex++;
+                        }
+
+                        // swap nums[i+1] and nums[fourIndex]
+                        int temp = nums[i + 1];
+                        nums[i + 1] = nums[fourIndex];
+                        nums[fourIndex] = temp;
+
+                        fourIndex++; // move one index for next search
+                }
+        }
+
+        return nums;
+}
+
+// Tested using CodingBat provided test cases
